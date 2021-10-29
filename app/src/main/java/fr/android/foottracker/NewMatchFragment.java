3a42afabcffdf18e36d95e7eef1d162606877749
Ewopro <ewopro12@gmail.com>
2021-10-29 15:37:16 +0200

    J'ai mis la maps dessus

--- app/src/main/java/fr/android/foottracker/NewMatchFragment.java
+++ app/src/main/java/fr/android/foottracker/NewMatchFragment.java
@@ -10,12 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class NewMatchFragment extends Fragment {
+public class NewMatchFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_newmatch, container, false);
+
+        Fragment fragment = new MapsFragment();
+
+        getChildFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
         return view;
     }
 
