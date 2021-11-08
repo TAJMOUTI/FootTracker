fcf43a72a9d0e2ef4f8241198bf8faa481cabfd9
Ewopro <ewopro12@gmail.com>
2021-11-08 17:03:04 +0100

    La map et la géoloc fonctionne l'ami

--- app/src/main/java/fr/android/foottracker/NewMatchFragment.java
+++ app/src/main/java/fr/android/foottracker/NewMatchFragment.java
@@ -1,26 +1,128 @@
 package fr.android.foottracker;
 
+import static android.content.ContentValues.TAG;
+import static android.content.Context.LOCATION_SERVICE;
+
+import android.annotation.SuppressLint;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class NewMatchFragment extends Fragment{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class NewMatchFragment extends Fragment implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private LocationManager lm;
+    Button btnShowAddress;
+    TextView tvAddress;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_newmatch, container, false);
 
-        Fragment fragment = new MapsFragment();
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
 
-        getChildFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
+        supportMapFragment.getMapAsync(this);
+
+        View mapView = supportMapFragment.getView();
+        mapView.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = mapView.getMeasuredWidth();
+                int height = mapView.getMeasuredHeight();
+
+                String widthAndHeight = width + " " + height;
+            }
+        });
+
+        lm = (LocationManager) view.getContext().getSystemService(LOCATION_SERVICE);
+        tvAddress = view.findViewById(R.id.localisation);
+
         return view;
+    }
+
+    @SuppressLint("MissingPermission")
+    public void onResume() {
+        super.onResume();
+
+        // 2 - register to receive the location events before the activity becomes visible
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 15000, 10, this);
+    }
+
+
+    public void onPause() {
+        super.onPause();
+
+        // 4- unregister from the service when the activity becomes invisible
+        lm.removeUpdates(this);
+    }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+
+    public void onLocationChanged(@NonNull Location location) {
+        // 3- received a new location from the GPS
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        String result_ville;
+        String result_postal;
+        String result_country;
+
+        // Add a marker and move the camera
+        LatLng newPos = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(newPos));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(newPos));
+
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+        String result = null;
+        try {
+            List<Address> addressList = geocoder.getFromLocation(
+                    lat, lng, 1);
+            if (addressList != null && addressList.size() > 0) {
+                Address address = addressList.get(0);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                    sb.append(address.getAddressLine(i)).append("\n");
+                }
+                sb.append(address.getLocality()).append("\n");
+                sb.append(address.getPostalCode()).append("\n");
+                sb.append(address.getCountryName());
+                result = sb.toString();
+                System.out.println(result);
+                tvAddress.setText(result);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Unable connect to Geocoder", e);
+        }
     }
 
 
